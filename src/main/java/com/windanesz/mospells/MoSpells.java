4b30362778395e3b9d98927c37b96663db3e690e
WinDanesz <31292708+WinDanesz@users.noreply.github.com>
2022-04-05 14:29:16 +0200

    chore: v1.0.3

--- src/main/java/com/windanesz/mospells/MoSpells.java
+++ src/main/java/com/windanesz/mospells/MoSpells.java
@@ -21,7 +21,7 @@ public class MoSpells {
 
 	public static final String MODID = "mospells";
 	public static final String MOD_NAME = "Mo' Spells";
-	public static final String VERSION = "1.0.2";
+	public static final String VERSION = "1.0.3";
 
 	/**
 	 * This is the instance of your mod as created by Forge. It will never be null.
